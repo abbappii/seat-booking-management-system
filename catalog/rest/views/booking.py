127144608c9c961ed70c49f62748a95d50989519
abbappii <bappi142434@gmail.com>
2024-08-28 16:04:30 +0600

    permission class for booking

--- catalog/rest/views/booking.py
+++ catalog/rest/views/booking.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from ...models import Booking
 from ..serializers.booking import BookingSerializer
@@ -8,13 +9,15 @@ API Views
     - Booking CRUD operation
 """
 
+# applying permission for booking need a user, who is booking this ticket
 
 class BookingList(generics.ListCreateAPIView):
     queryset = Booking.objects.filter()
     serializer_class = BookingSerializer
-
+    permission_classes = [IsAuthenticated]
 
 class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Booking.objects.filter()
     serializer_class = BookingSerializer
     lookup_field = "id"
+    permission_classes = [IsAuthenticated]
